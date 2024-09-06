02e9c6bba4fe4bb399f8a157f5a6f88331f15496
AdityaKumar-Git <adityak.nitrr@gmail.com>
2024-09-07 02:53:49 +0530

    Updated header for better responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,46 @@
-import React from 'react'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
-      <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="/">LCAnalyser</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
-            <Nav.Link href="/analyzer">Analyzer</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+      <div className='bg-zinc-800 min-h-16 flex justify-between px-4 items-center'>
+        <div className='text-center font-serif text-white'> LCAnalyser</div>
+        <div className='block lg:hidden'>
+          <button onClick={toggleMenu} className='text-white'>
+            <FontAwesomeIcon icon={isOpen ? faTimes : faBars} />
+          </button>
+        </div>
+        <nav className='hidden lg:flex flex-row lg:space-x-4 text-white'>
+          <a href="/" className='ml-2 no-underline hover:underline text-white'>Home</a>
+          <a href="/about" className='ml-2 no-underline hover:underline text-white'>About</a>
+          <a href="/contact" className='ml-2 no-underline hover:underline text-white'>Contact</a>
+          <a href="/analyzer" className='ml-2 no-underline hover:underline text-white'>Analyzer</a>
+        </nav>
+      </div>
+      {/* Side Panel for Mobile */}
+      <div className={`fixed top-0 right-0 w-64 h-full bg-zinc-900 text-white transform ${isOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform lg:hidden`}>
+        <div className='flex justify-end p-4'>
+          <button onClick={toggleMenu} className='text-white'>
+            <FontAwesomeIcon icon={faTimes} />
+          </button>
+        </div>
+        <nav className='flex flex-col p-4 space-y-4'>
+          <a href="/" className='no-underline hover:underline text-white' onClick={() => setIsOpen(false)}>Home</a>
+          <a href="/about" className='no-underline hover:underline text-white' onClick={() => setIsOpen(false)}>About</a>
+          <a href="/contact" className='no-underline hover:underline text-white' onClick={() => setIsOpen(false)}>Contact</a>
+          <a href="/analyzer" className='no-underline hover:underline text-white' onClick={() => setIsOpen(false)}>Analyzer</a>
+        </nav>
+      </div>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
